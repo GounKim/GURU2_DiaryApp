d1ec083d20414db8f7f3879947a00677f58716e0
enyul3417 <49514531+enyul3417@users.noreply.github.com>
2021-02-03 16:41:54 +0900

    일기 보는 화면 추가

---
+++ app/src/main/java/com/example/guru2_diaryapp/DiaryView.kt
@@ -0,0 +1,11 @@
+package com.example.guru2_diaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiaryView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.diary_view)
+    }
+}
