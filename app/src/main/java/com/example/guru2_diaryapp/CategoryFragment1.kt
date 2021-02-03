99cee57e9642d71d6ec5caba6cbc99ae16b29a6e
enyul3417 <49514531+enyul3417@users.noreply.github.com>
2021-02-03 21:02:38 +0900

    카테고리 제작 중 2

---
+++ app/src/main/java/com/example/guru2_diaryapp/CategoryFragment1.kt
@@ -0,0 +1,30 @@
+package com.example.guru2_diaryapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_category1.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CategoryFragment1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CategoryFragment1 : Fragment() {
+    var name = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_category1, container, false)
+        view.fragment_tv.text = name;
+    }
+}
