a7be3c886f5dbeec52708cdc7334940fea940eed
enyul3417 <49514531+enyul3417@users.noreply.github.com>
2021-02-04 16:51:02 +0900

    메인으로 돌아가는 버튼 + 기능 구현

--- app/src/main/java/com/example/guru2_diaryapp/DiaryView.kt
+++ app/src/main/java/com/example/guru2_diaryapp/DiaryView.kt
@@ -1,11 +1,31 @@
 package com.example.guru2_diaryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class DiaryView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.diary_view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_top, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            // 메인 화면으로 이동
+            R.id.action_main -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
