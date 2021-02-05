58db18e8ca373298678d479d1b1265ce04b53ecd
enyul3417 <49514531+enyul3417@users.noreply.github.com>
2021-02-05 15:43:35 +0900

    일기 편집 화면 - 갤러리에서 사진 불러오기 기능 구현

--- app/src/main/java/com/example/guru2_diaryapp/diaryView/DiaryViewEdit.kt
+++ app/src/main/java/com/example/guru2_diaryapp/diaryView/DiaryViewEdit.kt
@@ -1,27 +1,67 @@
 package com.example.guru2_diaryapp.diaryView
 
+import android.Manifest
+import android.R.attr
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.guru2_diaryapp.MainActivity
 import com.example.guru2_diaryapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class DiaryViewEdit : AppCompatActivity() {
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
+    private val REQUEST_CODE = 0
     lateinit var diary_et : EditText
+    lateinit var diary_bnv : BottomNavigationView
+    lateinit var image_preview : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.diary_view_edit)
 
         diary_et = findViewById(R.id.diary_et)
+        diary_bnv = findViewById(R.id.diary_bnv);
+        image_preview = findViewById(R.id.image_preview)
 
         loadDiary()
-    }
 
+        // 하단의 메뉴 선택될 때 호출될 리스너 등록
+        diary_bnv.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item?.itemId) {
+                R.id.mood -> {
+
+                }
+                R.id.weather -> {
+
+                }
+                R.id.current_time -> {
+
+                }
+                R.id.sticker -> {
+
+                }
+                R.id.image -> {
+                    selectGallery()
+                }
+            }
+            return@OnNavigationItemSelectedListener true
+        })
+
+
+    }
     // 상단에 메뉴
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_top, menu)
@@ -68,6 +108,54 @@ class DiaryViewEdit : AppCompatActivity() {
 
         if(content != "") {
             diary_et.setText(content.toString())
+        }
+    }
+
+    private fun selectGallery() {
+        // 앨범 접근 권한
+        var writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        var readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 허용되지 않음
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // 이전에 이미 권한이 거부되었을 때 설명
+                var dig = AlertDialog.Builder(this)
+                dig.setTitle("권한이 필요한 이유")
+                dig.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
+                dig.setPositiveButton("확인") { dialog, which ->
+                    ActivityCompat.requestPermissions(this@DiaryViewEdit,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+                }
+                dig.setNegativeButton("취소", null)
+                dig.show()
+            } else {
+                // 처음 권한 요청
+                ActivityCompat.requestPermissions(this@DiaryViewEdit,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            // 권한이 이미 허용됨
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(intent, REQUEST_CODE)
+
+        }
+    }
+
+    // 갤러리에서 사진 가져오기
+    override protected fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                data?.data?.let { uri ->
+                    image_preview.setImageURI(uri)
+                }!!
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
