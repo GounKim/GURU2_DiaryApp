e650b1a0548f30f0aceceb608b31b237242d0c50
enyul3417 <49514531+enyul3417@users.noreply.github.com>
2021-02-05 13:05:14 +0900

    일기편집 불러옴

--- app/src/main/java/com/example/guru2_diaryapp/category/CategoryFragment.kt
+++ app/src/main/java/com/example/guru2_diaryapp/category/CategoryFragment.kt
@@ -1,4 +1,4 @@
-package com.example.guru2_diaryapp
+package com.example.guru2_diaryapp.category
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.guru2_diaryapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
---
+++ app/src/main/java/com/example/guru2_diaryapp/diaryView/DiaryView.kt
@@ -0,0 +1,59 @@
+package com.example.guru2_diaryapp.diaryView
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.guru2_diaryapp.MainActivity
+import com.example.guru2_diaryapp.R
+
+class DiaryView : AppCompatActivity() {
+    lateinit var diary_tv : TextView
+    lateinit var diary_image : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.diary_view)
+
+        diary_tv = findViewById(R.id.diary_tv)
+        diary_image = findViewById(R.id.diary_image)
+
+        // 편집화면에서 작성한 글을 가져오기
+        var diary_text = intent.getStringExtra("diary_content")
+        if(diary_text == "") { // 가져온 것이 아무것도 없다면
+            diary_tv.text = "등록된 일기가 없습니다"
+        }
+        else {
+            diary_tv.text = diary_text
+        }
+
+        // 일기 편집 화면으로 이동
+        // 잘 작동되는지 테스트 필요
+        diary_tv.setOnClickListener {
+            val intent = Intent(this, DiaryViewEdit::class.java)
+            startActivity(intent)
+        }
+    }
+
+    // 상단에 메뉴
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_top, menu)
+        return true
+    }
+
+    // 상단 메뉴 작동
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            // 메인 화면으로 이동
+            R.id.action_main -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
