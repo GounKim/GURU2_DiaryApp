7f051607af101388f8f63bdb02fcafc888d60a3a
sungyoung441 <sunyong20831@gmail.com>
2021-02-06 14:59:05 +0900

    tracker 표 지운 것과 수정 반영
    정인님 코드를 이쪽으로 합쳤습니다.
    
    (cherry picked from commit 96580e3ae94cae8b4cd13c33a48ddc25ceaa176d)

--- app/src/main/java/com/example/guru2_diaryapp/Tracker.kt
+++ app/src/main/java/com/example/guru2_diaryapp/Tracker.kt
@@ -1,11 +1,34 @@
 package com.example.guru2_diaryapp
 
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Tracker : AppCompatActivity() {
+
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracker)
+
+
+
+        dbManager = DBManager(this,"habit_lists",null,1)
+        dbManager = DBManager(this,"habit_check_lists",null,1)
+
+        sqlitedb = dbManager.readableDatabase
+
+        var cursor : Cursor
+        cursor = sqlitedb.rawQuery("SELECT habit FROM habit_lists",null)
     }
+
+
+
+
 }
