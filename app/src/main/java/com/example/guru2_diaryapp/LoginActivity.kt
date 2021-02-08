41efe0bca904efc9ebba9566af5dee74afe2f459
Jungin-2 <ji991031@naver.com>
2021-02-08 15:14:24 +0900

    로그인 수정

--- app/src/main/java/com/example/guru2_diaryapp/LoginActivity.kt
+++ app/src/main/java/com/example/guru2_diaryapp/LoginActivity.kt
@@ -47,7 +47,7 @@ class LoginActivity : AppCompatActivity() {
 
         //Google 로그인 옵션 구성. requestIdToken 및 Email 요청
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken("472048867852-f0if4is70tou46km6eacbpei9t4auc2r.apps.googleusercontent.com")
                 .requestEmail()
                 .build()
 
