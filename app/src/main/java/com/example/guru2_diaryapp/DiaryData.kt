f4d7af0e3f255678c8cf7b056f1a5d0bdbbe1fec
sungyoung441 <sunyong20831@gmail.com>
2021-02-09 23:46:56 +0900

    1146

---
+++ app/src/main/java/com/example/guru2_diaryapp/DiaryData.kt
@@ -0,0 +1,6 @@
+package com.example.guru2_diaryapp
+
+class DiaryData (var id:Int, var reporting_date:Int, var weather:Int,
+                 var category_name:String,var content:String,
+                 var imgs:ArrayList<String>?){
+}
