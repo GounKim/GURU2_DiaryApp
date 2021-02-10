4bad13e61153250ae4d33393dadfe5410cc85397
sungyoung441 <sunyong20831@gmail.com>
2021-02-10 15:10:42 +0900

    diary_view / Edit xml 수정

--- app/src/main/java/com/example/guru2_diaryapp/diaryView/DiaryView.kt
+++ app/src/main/java/com/example/guru2_diaryapp/diaryView/DiaryView.kt
@@ -34,9 +34,15 @@ class DiaryView : AppCompatActivity() {
     lateinit var sqllitedb : SQLiteDatabase
     var postID : Int = 0
 
+    lateinit var toolbar:androidx.appcompat.widget.Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.diary_view)
+
+        //툴바 장착
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         diary_tv = findViewById(R.id.diary_tv)
         diary_image = findViewById(R.id.diary_image)
@@ -50,7 +56,7 @@ class DiaryView : AppCompatActivity() {
         //date_tv.text = intent.getStringExtra("select_date")
         newDate = intent.getIntExtra("newDate", 0)
 
-        loadDiary()
+        //loadDiary()
 
         // 선택한 카테고리 가져오기
         /*var category_text = intent.getStringExtra("selected_category")
