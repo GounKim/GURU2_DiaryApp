939c053e8982f7d69689d1266f4295acee49752c
GounKim <78197675+GounKim@users.noreply.github.com>
2021-02-12 22:39:44 +0900

    무드 설정 완료

--- app/src/main/java/com/example/guru2_diaryapp/Tracker/Tracker.kt
+++ app/src/main/java/com/example/guru2_diaryapp/Tracker/Tracker.kt
@@ -134,9 +134,6 @@ class Tracker : AppCompatActivity(),
 
                     if (str_habit == "mood") {
                         when (checkLevel) {
-                            0 -> {
-                                calendarView.addDecorator(MoodSick(this, CalendarDay.from(year, month - 1, day), checkLevel))
-                            }
                             1 -> {
                                 calendarView.addDecorator(MoodBadDeco(this, CalendarDay.from(year, month - 1, day), checkLevel))
                             }
@@ -145,6 +142,12 @@ class Tracker : AppCompatActivity(),
                             }
                             3 -> {
                                 calendarView.addDecorator(MoodGoodDeco(this, CalendarDay.from(year, month - 1, day), checkLevel))
+                            }
+                            4 -> {
+                                calendarView.addDecorator(MoodSickDeco(this, CalendarDay.from(year, month - 1, day), checkLevel))
+                            }
+                            5 -> {
+                                calendarView.addDecorator(MoodSurpriseDeco(this, CalendarDay.from(year, month - 1, day), checkLevel))
                             }
                         }
                     }
